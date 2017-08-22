9e5448257cab59787d8825df6d3cee4c4b9f3178
libai <libai@mindai.com>
2017-08-22 11:35:07 +0800

    创建一个服务ms-a

---
+++ src/main/java/com/pet/sample/ms_a/MSApplication.java
@@ -0,0 +1,13 @@
+package com.pet.sample.ms_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MSApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MSApplication.class, args);
+  }
+}
