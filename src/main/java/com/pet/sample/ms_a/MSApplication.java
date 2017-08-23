57c7c8e521ffd5c8e082dd0e9366f2022787b57a
libai <libai@mindai.com>
2017-08-23 11:46:47 +0800

    添加restful接口

--- src/main/java/com/pet/sample/ms_a/MSApplication.java
+++ src/main/java/com/pet/sample/ms_a/MSApplication.java
@@ -2,10 +2,9 @@ package com.pet.sample.ms_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class MSApplication {
   public static void main(String[] args) {
     SpringApplication.run(MSApplication.class, args);
